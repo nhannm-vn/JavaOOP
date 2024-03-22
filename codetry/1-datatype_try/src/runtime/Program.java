a84eb25cf6dd6652b6b83809fa3146cc522a92af
nhannm-vn <nhanmin911@gmail.com>
2024-03-23 00:18:24 +0700

    23-3-2024

---
+++ codetry/1-datatype_try/src/runtime/Program.java
@@ -0,0 +1,17 @@
+
+package runtime;
+
+
+public class Program {
+    public static void main(String[] args) {
+        String thongBao = "Xin Chào";
+        String hoVaTen = "Nguyễn Minh Nhân";
+        String tenSach;
+        tenSach  = "Lap trình Java";
+        System.out.println("họ và tên: " + hoVaTen);
+        System.out.printf("tên sách: %s", tenSach);
+        System.out.printf("\n");
+        System.out.println("thông báo: " + thongBao);
+        
+    }
+}
---
+++ codetry/2-useDataType/src/runtime/Program.java
@@ -0,0 +1,19 @@
+
+package runtime;
+
+
+public class Program {
+    public static void main(String[] args) {
+        String tenSach = "Lập trình Java";
+        int namXuatBan = 2021;
+        double giaTien = 15.5;
+        boolean conSach = true;
+        char maKho = 'M';
+        float tienThua = 12.252F;
+        System.out.println("Tên sách: " + tenSach);
+        System.out.println("Năm: " + namXuatBan);
+        System.out.println("Giá: " + giaTien );
+        System.out.println("Còn sách: " + conSach);
+        System.out.println("Mã Kho: " + maKho);
+    }
+}
