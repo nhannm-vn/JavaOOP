0543b58ffd5a4912dcc782432a0484751149d9c2
nhannm-vn <nhanmin911@gmail.com>
2024-05-14 18:25:51 +0700

    14-5-2024

--- codepied/15-tutorialCollection/src/runtime/Program.java
+++ codepied/15-tutorialCollection/src/runtime/Program.java
@@ -1,9 +1,14 @@
 package runtime;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /*
 Collection: là những cái mảng rất đặc biệt
@@ -23,8 +28,8 @@ khi không đúng thứ tự
 */
 public class Program {
     public static void main(String[] args) {
-        playWithArrayList();
-        
+        //playWithArrayList();
+        playWithQueue();
     }
     public static void playWithArrayList(){//build
     /*
@@ -108,7 +113,7 @@ public class Program {
         //arrList[3 5 7]
         System.out.println(arrList.get(1));//được 5 vì nó nằm ở vị trí số 1
         
-        //.set(index, vakue): gán | thay thế giá trị ở vị trí index bằng value
+        //.set(index, value): gán | thay thế giá trị ở vị trí index bằng value
         //muốn đổi số 5 thành số 9 
         arrList.set(1, 9);//đổi 5 thành 9 
         //arrList[3 9 7]
@@ -167,6 +172,100 @@ public class Program {
         //.isEmpty(): có trống hay không(boolean)
         //.indexOf(key): tìm key ở vị trí nào
         //.trimToSize(): thu nhỏ đến kích thước thật
+        
+    }
+    
+    
+    public static void playWithQueue(){
+        Queue<Integer> numList = new LinkedList<>();
+        //khai cha new con
+        //.add(value)
+        //.add(index, value): nhét phần tử vào phần tail của danh sách(true|illegal exception)
+        numList.add(3);//không có .add(index,value) vì nếu như vậy là chen hàng
+        //.offer(): nhét phần tử vào tail của danh sách(true|false);
+        numList.offer(1);
+        numList.offer(5);
+        numList.offer(4);
+        //numList: head[3 1 5 4]tail
+        //vẫn giữ được thứ tự truyền vào vì linkedlist thuộc list
+        //hàng đợi thì không có get(index) ==> k được phép lấy ra phần tử bất kỳ trong mảng
+        
+        //.element(): lấy ra xem pt đầu tiên ở head(pt|null)
+        numList.element();//chỉ nhìn thấy số 3
+        numList.element();//chỉ nhìn thấy số 3
+        //.peek(): nhìn trộm, lấy ra xem pt đầu tiên ở head(pt|null)
+        numList.peek();//số 3
+        
+        //.remove(): xóa pt đầu tiên ở vị trí head và return pt đã xóa => mình lấy
+        //ra để xử lý(pt|nullPointer|exception)
+        
+        numList.remove();//xóa số 3 và ném số 3 ra cho mình xử lí [1 5 4]
+        numList.remove();//xóa số 1 và ném ra cho mình xử lí[5 4]
+        //.poll(): láy phân tử đầu tiên của head ra khỏi danh sách(pt|null)
+        numList.poll();//lấy số 5 khỏi danh sách nên còn [4]
+        
+        System.out.println(numList);
+        
+        //nhét thêm vài số vào lại
+        numList.offer(5);
+        numList.offer(1);
+        numList.offer(2);
+        //[4 5 1 2]
+        //thường thì Queue không quan tâm đến bên trong chứa gì cả
+        //k quan tâm đến số lượng bao nhiêu, chỉ quan tâm là thằng tiếp theo 
+        //còn hay là không, k thì kết thúc
+        //mô phỏng xử lí hàng đợi
+        while(true){
+            Integer tmp = numList.poll();//lấy thằng đầu hàng của numList
+            if(tmp == null) break;//thoát nếu không còn ai trong danh sách
+            //nếu có thì do something
+            System.out.println("Ahihi" + tmp + "là đứa bị xử lí tiếp theo");
+        }
+        //numList sẽ là []
+        
+        //Priority queue: hàng đợi ưu tiên (min heap)
+        Queue<Integer> numListV2 = new PriorityQueue<>();
+        numListV2.offer(3);
+        numListV2.offer(1);
+        numListV2.offer(5);
+        numListV2.offer(9);
+        numListV2.offer(2);
+        
+        System.out.println(numListV2);
+        
+        //rút poll();
+        numListV2.poll();//1 [2 3 5 9]
+        numListV2.poll();//2 [3 9 5]
+        numListV2.poll();//3 [5 9]
+        numListV2.poll();//5 [9]
+        numListV2.poll();//9 []
+        System.out.println(numListV2);
+        
+        //ArrayDeque: mạnh nhất nhà Deque
+        //được phép ra vào 2 đầu. nên chỉ có tên front và rear
+        Deque<Integer> numDeque = new ArrayDeque<>();
+        //front(first) ... rear(last)
+        //.add()|.offer()|.addLast()|.offerLast() nhét vào rear
+        //.addFirst()|offerFirst(): nhét vào front
+        
+        //nhìn
+        //.element()|.peek()|.elementFirst()|.peekFirst(): nhìn ở front
+        //.elementLast()|peekLast(): nhìn ở rear
+        
+         //neft sức mạnh
+        //muốn dùng arrayDeque để mô tả 1 queue FIFO(vào tail ra head)
+        //front(first)...rear(last)
+        //thêm: .offerLast()
+        //nhìn: .peekFirst()
+        //lấy: .pollFirst()
+        
+        
+        //muốn dùng arrayDeque để mô tả 1 stack FIFO(vào tail ra tail)
+        //front(first)...rear(last)
+        //thêm: .offerLast()
+        //nhìn: .peekLast()
+        //lấy: .pollLast()
+        
         
     }
 }
